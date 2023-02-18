6572baaed6d93f84d7fd08e8284506e331a07cb9
santo <santosh1234aryal@gmail.com>
2023-02-18 13:26:07 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StackSearch.Models;
+
+namespace StackSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly StackOverflowDbContext _context;
+
+
+        public HomeController(ILogger<HomeController> logger, StackOverflowDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> showSearchResult(String searchPhrase)
+        {
+            return View("showSearchResult", await _context.Posts.Where ( j => j.Title.Contains(searchPhrase)).Take(10).ToListAsync());
+        }
+
+		public async Task<IActionResult> ViewUser(String postOwnerId)
+		{
+			return View("ViewUser", await _context.Users.Where(j => j.Id.Equals(int.Parse(postOwnerId))).ToListAsync());
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StackOverflowDbContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StackSearch.Models;
+
+public partial class StackOverflowDbContext : DbContext
+{
+    public StackOverflowDbContext()
+    {
+    }
+
+    public StackOverflowDbContext(DbContextOptions<StackOverflowDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Badge> Badges { get; set; }
+
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<VoteType> VoteTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source = LAPTOP-6EMGJPI3\\MSSQLSERVER01;Initial Catalog=StackOverflow2010;Integrated Security=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Badge>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Badges__Id");
+
+            entity.Property(e => e.Date).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(40);
+        });
+
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Posts__Id");
+
+            entity.Property(e => e.ClosedDate).HasColumnType("datetime");
+            entity.Property(e => e.CommunityOwnedDate).HasColumnType("datetime");
+            entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            entity.Property(e => e.LastActivityDate).HasColumnType("datetime");
+            entity.Property(e => e.LastEditDate).HasColumnType("datetime");
+            entity.Property(e => e.LastEditorDisplayName).HasMaxLength(40);
+            entity.Property(e => e.Tags).HasMaxLength(150);
+            entity.Property(e => e.Title).HasMaxLength(250);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Users_Id");
+
+            entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            entity.Property(e => e.DisplayName).HasMaxLength(40);
+            entity.Property(e => e.EmailHash).HasMaxLength(40);
+            entity.Property(e => e.LastAccessDate).HasColumnType("datetime");
+            entity.Property(e => e.Location).HasMaxLength(100);
+            entity.Property(e => e.WebsiteUrl).HasMaxLength(200);
+        });
+
+
+        modelBuilder.Entity<VoteType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_VoteType__Id");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
